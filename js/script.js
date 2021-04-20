8f174cbc821697ed048df0e828d2bf7e2aacafa9
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-04-20 17:09:26 +0200

    fine slider

--- js/script.js
+++ js/script.js
@@ -2,7 +2,33 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            
+            activeImage: 0,
+            linksImages: [
+                'img/campagna.jpg',
+                'img/cascate.webp',
+                'img/fiori.jpeg',
+                'img/montagne.webp'
+            ]
+        },
+        methods: {
+            nextImage() {
+                const nextImageIndex = this.activeImage + 1;
+
+                if ( nextImageIndex > this.linksImages.length - 1 ) {
+                    this.activeImage = 0;
+                } else {
+                    this.activeImage = nextImageIndex;
+                }
+            },
+            prevImage() {
+                const prevImageIndex = this.activeImage - 1;
+
+                if ( prevImageIndex < 0) {
+                    this.activeImage = this.linksImages.length - 1;
+                } else {
+                    this.activeImage = prevImageIndex;
+                }
+            }
         }
     }
 );
